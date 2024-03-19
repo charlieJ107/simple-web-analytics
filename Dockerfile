072fd491aa7cedcb68e85700ee091776ece2973a
CharlieJ107 <charlie_j107@outlook.com>
2024-03-19 21:28:53 +0000

    Add config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:alpine
 LABEL authors="CharlieJ107 <charlie_j107@outlook.com>"
-RUN pip install Flask
 COPY . /app
-ENTRYPOINT ["top", "-b"]
+WORKDIR /app
+RUN python -m pip install --no-cache-dir -r  requirements.txt
+EXPOSE 80
+CMD ["python" , "-m", "gunicorn ", "-w", "4", "-b", "0.0.0.0:80", "app:app"]
--- v1/report.py
+++ v1/report.py
@@ -1,18 +1,48 @@
 from flask import Blueprint, request
+import os
+import json
+from os import path
+import datetime
 
-v1bp = Blueprint('web-analysis-v1', "BlueprintV1", url_prefix="/v1",
+v1bp = Blueprint('web-analysis-v1', "BlueprintV1", url_prefix="/api/v1",
                  template_folder='templates')
 
 
 @v1bp.route('/report', methods=['POST'])
 def report():
-    request_data = request.get_json()
-    cookies = request.cookies
+    request_data = json.dumps(request.get_json())
+    # Get cookies from the request, convert it to a json string
+    cookies = json.dumps(request.cookies)
     ua = request.user_agent
     ip = request.remote_addr
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     # Write these data into a csv file under the /tmp/swa/report-<date>.csv
     # The csv file should have the following columns:
     # ip, user_agent, cookies, request_data
-    with open('/tmp/swa/report-<date>.csv', 'a') as f:
-        f.write(f"{ip},{ua},{cookies},{request_data}\n")
+
+    # Read report_path from config.json
+    with open('config.json') as f:
+        config = json.load(f)
+        # if on windows
+        if os.name == 'nt':
+            report_path = config['windows']['v1']['report_path']
+        # if on linux
+        elif os.name == 'posix':
+            report_path = config['linux']['v1']['report_path']
+
+    # Check if the file exists, if not create it
+    if not os.path.exists(report_path):
+        os.makedirs(report_path)
+    # Build report file name with datetime, yyyy-mm-dd:
+    report_file_name = f"report-{datetime.datetime.now().strftime('%Y-%m-%d')}.csv"
+    report_file_path = path.join(report_path, report_file_name)
+    if not os.path.exists(report_file_path):
+        with open(report_file_path, 'w') as f:
+            f.write("ip,user_agent,cookies,request_data\n")
+            f.write(f"{ip},{ua},{cookies},{request_data},{timestamp}\n")
+    else:
+        with open(report_file_path, 'a') as f:
+            f.write(f"{ip},{ua},{cookies},{request_data},{timestamp}\n")
+
     return "OK"
